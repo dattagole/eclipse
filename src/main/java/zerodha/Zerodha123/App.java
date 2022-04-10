1f4cb11a46257704f3f6b81da6f5e1b9bfc9061f
Datta Gole <dattagole93@gmail.com>
2022-04-10 19:28:43 +0530

    Done

---
+++ src/main/java/zerodha/Zerodha123/App.java
@@ -0,0 +1,13 @@
+package zerodha.Zerodha123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
